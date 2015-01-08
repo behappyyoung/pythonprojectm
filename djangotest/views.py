e38635e48a193fcbc94bfc4a7b8b8b25672f1d17
young <behappydev@gmail.com>
2015-01-07 16:09:30 -0800

    update 2015-01-07-16:09:30 -

--- djangotest/views.py
+++ djangotest/views.py
@@ -2,12 +2,16 @@ __author__ = 'young'
 
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-
-
+from django.utils import timezone
+from django.views import generic
 from meals.models import Meal, Dish
 
 
-def IndexView(request):
+class HomeView(generic.ListView):
+    template_name = 'home/index.html'
     context_object_name = 'meal_list'
-    model = Meal
-    return render_to_response('home/index.html', context_instance=RequestContext(request))
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        ##return Question.objects.order_by('-pub_date')[:5]
+        return Meal.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
